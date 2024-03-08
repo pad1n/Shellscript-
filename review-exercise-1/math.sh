4ba17753c7da63bf1dfa69ea2477992eaeb037aa
Rodrigo Padilha <151454973+pad1n@users.noreply.github.com>
2024-03-08 09:09:47 -0300

    Add files via upload
    Script básico que faça a soma de dois números escritos pelo usuário e, depois, faça o produto do primeiro pelo segundo + 2.

---
+++ review-exercise-1/math.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+a1=$1
+a2=$2
+
+echo -e "Script para soma de dois números\n"
+read -p "Digite o primeiro número: " a1
+read -p "Digite o segundo número: " a2
+sleep 1
+echo -e "\nA soma do primeiro número com o segundo número é: $(( $a1 + $a2 ))\n"
+sleep 1
+echo -e "O produto do primeiro pelo segundo +2: $(($a1 * $a2 + 2))"
