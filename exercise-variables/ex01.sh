9041f24b96660bc01dd084f792b99309ec5630e1
pad1n <rodripadidev@gmail.com>
2024-03-10 10:40:48 -0300

    Pequeno exercício adicionado!

---
+++ exercise-variables/ex01.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#read -p "Digite o dia da semana: " dia
+#read -p "Digite o mês do ano: " mes
+#read -p "Digite o ano: " ano
+
+echo "Primeira data:" "$1/$2/$3"
+echo "Segunda data:" "$4/$5/$6"
